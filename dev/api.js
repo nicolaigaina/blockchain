295732057ed2ce1cc0c457c9c2ba9f2e2cd27f73
Nicolai Gaina <nicolaigaina@gmail.com>
2019-01-10 04:38:20 -0800

    more endpoints

--- dev/api.js
+++ dev/api.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain')
 const PORT = 3000;
 const HOST = 'localhost';
 
+const blockchain = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/blockchain', function (req, res) {
-  res.send('Hello World');
+  res.send(blockchain);
 });
 
 app.post('/transaction', function (req, res) {
-  res.send('Hello World');
+  res.send(`The amount of the transaction is ${req.body.amount} bitcoin.`);
 });
 
 app.get('/mine', function (req, res) {
